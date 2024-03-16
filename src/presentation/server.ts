2b0bd6711d6b921686cfe928287ca2f2c53fd900
Francisco <31752492+PanchonSoto@users.noreply.github.com>
2024-03-15 19:19:15 -0700

    json server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -12,6 +12,7 @@ export class Server {
             '*/5 * * * * *',
             ()=>{
                 new CheckService().execute('https://google.com');
+                // new CheckService().execute('http://localhost:3000/');
             }
         );
     }
