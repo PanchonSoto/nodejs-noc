df58a7e40eb5875d3179eb52738a29d936b14acf
Francisco <31752492+PanchonSoto@users.noreply.github.com>
2024-04-01 11:42:24 -0700

    mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 
 import { envs } from "./config/plugins/envs.plugins";
+import { MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
 
@@ -10,7 +11,11 @@ import { Server } from "./presentation/server";
 
 
 
-function main() {
+async function main() {
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    });
     Server.start();
     // console.log(envs);
 }
