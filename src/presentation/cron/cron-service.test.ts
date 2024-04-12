c10a131cc286feb0870962906283355ee8839b5a
Francisco Soto <panchon.ues@gmail.com>
2024-04-12 04:15:20 -0700

    cron and email service test

---
+++ src/presentation/cron/cron-service.test.ts
@@ -0,0 +1,17 @@
+import { CronService } from "./cron-service";
+
+const mockTick = jest.fn();
+
+describe('CronService ',() => {
+
+    test('should create a job',(done)=>{
+        const job = CronService.createJob('* * * * * *', mockTick);
+
+        setTimeout(() => {
+            expect(mockTick).toHaveBeenCalledTimes(2);
+            job.stop();
+            done();
+        }, 2000);
+    });
+
+});
